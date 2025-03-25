b1b2bb7b0a58e00ba5eda63391edf548e801a0a5
BioTomateDE <biotomatede@proton.me>
2025-03-25 13:51:23 +0100

    FUNCITONS work now (kinda)

--- src/deserialize/functions.rs
+++ src/deserialize/functions.rs
@@ -1,15 +1,16 @@
-﻿use crate::chunk_reading::UTChunk;
+﻿use std::collections::HashSet;
+use crate::chunk_reading::UTChunk;
 use crate::deserialize::strings::UTStrings;
+use crate::printing::hexdump;
 
 #[derive(Debug, Clone)]
 pub struct UTFunction {
     pub name: String,
-    pub occurrences: u32,
-    pub first_occurrence: u32,      // pointer to some location in code (position relative/absolute to data.win)
+    pub occurrences: HashSet<usize>,                // set of occurrences (call instructions) positions relative to chunk CODE
 }
 
 // pub struct UTFunctions {
-//     functions: Vec<UTFunction>,
+//     functions: HashMap<usize, UTFunction>,      // maps occurrence positions (relative to chunk CODE) to a function
 // }
 
 #[derive(Debug)]
@@ -22,18 +23,18 @@ pub struct UTCodeLocal {
     pub variables: Vec<UTCodeLocalVariable>,
 }
 
-pub fn parse_chunk_FUNC(mut chunk: UTChunk, strings: &UTStrings) -> Result<(Vec<UTFunction>, Vec<UTCodeLocal>), String> {
+pub fn parse_chunk_FUNC(mut chunk: UTChunk, strings: &UTStrings, chunk_CODE: &UTChunk) -> Result<(Vec<UTFunction>, Vec<UTCodeLocal>), String> {
     let functions_length: usize = chunk.read_usize()?;
     let mut functions: Vec<UTFunction> = Vec::with_capacity(functions_length);
 
     for _ in 0..functions_length {
         let function_name: String = chunk.read_ut_string(strings)?;
-        let occurrences: u32 = chunk.read_u32()?;   // idk what this values actually represents
-        let first_occurrence: u32 = chunk.read_u32()?;
+        let occurrence_count: usize = chunk.read_usize()?;
+        let first_occurrence: i32 = chunk.read_i32()? - chunk_CODE.abs_pos as i32;
+        let occurrences: HashSet<usize> = get_occurrences(occurrence_count, first_occurrence, chunk_CODE);
         let function: UTFunction = UTFunction {
             name: function_name,
             occurrences,
-            first_occurrence,
         };
         functions.push(function);
     }
@@ -65,7 +66,7 @@ pub fn parse_chunk_FUNC(mut chunk: UTChunk, strings: &UTStrings) -> Result<(Vec<
     }
 
     // for i in &functions {
-    //     println!("[Function]    {:<32} | {:<4} | {}", i.name, i.occurrences, i.first_occurrence);
+    //     println!("[Function]    {:<32} | {:<4} | {:?}", i.name, i.occurrences.len(), i.occurrences);
     // }
     // for i in &code_locals {
     //     println!("[Code Local]    {:<48} | {:?}", i.name, i.variables);
@@ -74,3 +75,44 @@ pub fn parse_chunk_FUNC(mut chunk: UTChunk, strings: &UTStrings) -> Result<(Vec<
     Ok((functions, code_locals))
 }
 
+
+fn get_occurrences(count: usize, first_occurrence: i32, chunk_CODE: &UTChunk) -> HashSet<usize> {
+    let mut occurrences: HashSet<usize> = HashSet::new();
+    let mut occurrence: i32 = first_occurrence + 4;
+
+    let mut i = 0;
+
+    for _ in 0..count {
+        // println!("occ {occurrence} | {}", chunk_CODE.abs_pos);
+        // occurrence -= chunk_CODE.abs_pos as i32;
+        occurrences.insert(occurrence as usize - 4);
+        // TODO index safety
+        let raw: [u8; 4] = chunk_CODE.data[(occurrence as usize) .. (occurrence as usize)+4].try_into().unwrap();
+        // println!("{}", hexdump(&chunk_CODE.data, (occurrence as usize)-4, Some(occurrence as usize+4)).unwrap());
+        if chunk_CODE.data[occurrence as usize - 1] != 0xD9 {
+            break;
+        }
+        occurrence += i32::from_le_bytes(raw);
+        i += 1;
+    }
+
+    // println!("FUNCITONSBDF | expected: {count:<10},  actual: {i:<10}   {}", count==i);
+
+    occurrences
+}
+
+
+pub fn get_function(functions: &[UTFunction], position: usize) -> Result<UTFunction, String> {
+    for function in functions {
+        // println!("{:<30} {:?}", function.name, function.occurrences);
+        if function.occurrences.contains(&position) {
+            // lowkey disgusting solution but whatever i need to save memory
+            return Ok(UTFunction {
+                name: function.name.clone(),
+                occurrences: HashSet::with_capacity(0)
+            });
+            // return Ok(function.clone());
+        }
+    }
+    Err(format!("Could not find function for position {position} (len functions: {}).", functions.len()))
+}
