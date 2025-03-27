a362238c20e31e62de939af11e7428ccd8175ff1
BioTomateDE <biotomatede@proton.me>
2025-03-27 16:53:20 +0100

    fonts idk

---
+++ src/serialize/fonts.rs
@@ -0,0 +1,102 @@
+use crate::deserialize::all::UTData;
+use crate::deserialize::chunk_reading::UTChunk;
+use crate::deserialize::fonts::UTGlyph;
+use crate::serialize::all::{build_chunk, DataBuilder};
+use crate::serialize::chunk_writing::ChunkBuilder;
+
+pub fn build_chunk_FONT(data_builder: &mut DataBuilder, ut_data: &UTData) -> Result<(), String> {
+    let mut builder: ChunkBuilder = ChunkBuilder { raw_data: Vec::new(), chunk_name: "FONT" };
+
+    let font_count: usize = ut_data.fonts.len();
+    builder.write_usize(font_count)?;
+
+    // write placeholder bytes for font absolute positions
+    for _ in 0..font_count {
+        builder.write_usize(0)?;
+    }
+
+    for (i, font) in ut_data.fonts.iter().enumerate() {
+        let absolute_position: usize = data_builder.len() + builder.len();
+        // write font absolute position to placeholder bytes
+        let bytes: [u8; 4] = (absolute_position as u32).to_le_bytes();
+        builder.overwrite_data(&bytes, i * 4 + 4)?;
+
+        builder.write_string(&font.name)?;
+        builder.write_string(&font.display_name)?;
+        builder.write_u32(font.em_size)?;
+        builder.write_u32(if font.bold {1} else {0})?;
+        builder.write_u32(if font.italic {1} else {0})?;
+        builder.write_u16(font.range_start)?;
+        builder.write_u8(font.charset)?;
+        builder.write_u8(font.anti_alias)?;
+        builder.write_u32(font.range_end)?;
+        builder.write_u32(font.texture)?;   // REPLACE WITH TexturePageItem WHEN AVAILABLE
+        builder.write_f32(font.scale_x)?;
+        builder.write_f32(font.scale_y)?;
+
+        // maybe check version instead of checking if not none? (probably ok if handled correctly by mod versions)
+        match font.ascender_offset {
+            Some(number) => builder.write_i32(number)?,
+            None => (),
+        };
+        match font.ascender {
+            Some(number) => builder.write_u32(number)?,
+            None => (),
+        };
+        match font.sdf_spread {
+            Some(number) => builder.write_u32(number)?,
+            None => (),
+        };
+        match font.line_height {
+            Some(number) => builder.write_u32(number)?,
+            None => (),
+        };
+
+        build_glyphs(data_builder, &mut builder, &font.glyphs, &font.name)?;
+    }
+
+    build_chunk(data_builder, builder)?;
+    Ok(())
+}
+
+
+fn build_glyphs(data_builder: &DataBuilder, builder: &mut ChunkBuilder, glyphs: &[UTGlyph], font_name: &str) -> Result<(), String> {
+    builder.write_usize(glyphs.len())?;
+
+    // write placeholder bytes for glyph absolute positions
+    let placeholder_position: usize = data_builder.len() + builder.len();
+    for _ in 0..glyphs.len() {
+        builder.write_usize(0)?;
+    }
+
+    for (i, glyph) in glyphs.iter().enumerate() {
+        let absolute_position: usize = data_builder.len() + builder.len();
+        // write font absolute position to placeholder bytes
+        let bytes: [u8; 4] = (absolute_position as u32).to_le_bytes();
+        builder.overwrite_data(&bytes, placeholder_position + i*4)?;
+
+        let character: u16 = match glyph.character.try_into() {
+            Ok(ch) => ch,
+            Err(_) => return Err(format!(
+                "Unable to fit character '{}' (0x{:04X}) into 16 bits \
+                (which is required by GameMaker) for glyph with index {} \
+                in font \"{}\".",
+                glyph.character,
+                glyph.character as u32,
+                i,
+                font_name
+            ))
+        };
+        builder.write_u16(character)?;
+        builder.write_u16(glyph.x)?;
+        builder.write_u16(glyph.y)?;
+        builder.write_u16(glyph.width)?;
+        builder.write_u16(glyph.height)?;
+        builder.write_i16(glyph.shift_modifier)?;
+        builder.write_i16(glyph.offset)?;
+        builder.write_u16(0)?;      // kerning
+    }
+
+    Ok(())
+}
+
