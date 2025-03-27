999de46be10616cfb9fa1b80fac3ef37e2aed959
biotomatede <latus.kati@gmail.com>
2025-03-27 16:53:20 +0100

    fonts idk

--- src/serialize/chunk_writing.rs
+++ src/serialize/chunk_writing.rs
@@ -1,12 +1,13 @@
 
 
 #[derive(Debug, Clone)]
-pub struct DataBuilder {
+pub struct ChunkBuilder {
     pub raw_data: Vec<u8>,
+    pub chunk_name: &'static str,
 }
 
 
-impl DataBuilder {
+impl ChunkBuilder {
     pub fn write_u64(&mut self, number: u64) -> Result<(), String> {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
@@ -61,6 +62,12 @@ impl DataBuilder {
         }
         Ok(())
     }
+    pub fn write_f32(&mut self, number: f32) -> Result<(), String> {
+        for byte in number.to_le_bytes() {
+            self.raw_data.push(byte);
+        }
+        Ok(())
+    }
     pub fn write_bool(&mut self, boolean: bool) -> Result<(), String> {
         let number: u8 = if boolean {1} else {0};
         for byte in number.to_le_bytes() {
@@ -73,12 +80,30 @@ impl DataBuilder {
         for (i, char) in string.chars().enumerate() {
             let byte: u8 = match char.try_into() {
                 Ok(byte) => byte,
-                Err(_) => return Err(format!("Char Typecasting error while writing string \"{string}\" (i: {i}) to data (len: {})", self.raw_data.len())),
-                // Err(_) => return Err(format!("Non-Ascii character at position {i} in string \"{string}\" (data len: {}).", self.raw_data.len())),
+                Err(_) => return Err(format!("Char Typecasting error while writing string \"{string}\" (i: {i}) to chunk (len: {})", self.len())),
             };
             self.raw_data.push(byte);
         }
         Ok(())
+    }
+    pub fn overwrite_data(&mut self, data: &[u8], position: usize) -> Result<(), String> {
+        if position + data.len() >= self.len() {
+            return Err(format!(
+                "Could not overwrite {} bytes at position {} in data with length {} while building chunk.",
+                data.len(),
+                position,
+                self.len()
+            ))
+        };
+
+        for (i, byte) in data.iter().enumerate() {
+            self.raw_data[position + i] = *byte;
+        }
+
+        Ok(())
+    }
+    pub fn len(&self) -> usize {
+        self.raw_data.len()
     }
 }
 
