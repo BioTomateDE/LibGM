c1ff7f1926e2ed7e96883dabcc001499582c5bab
cyhp_tomate <bio@tomate.de>
2025-04-23 17:54:57 +0200

    serizalize snake case, no more question marks

--- src/serialize/chunk_writing.rs
+++ src/serialize/chunk_writing.rs
@@ -9,72 +9,61 @@ pub struct ChunkBuilder {
 
 
 impl ChunkBuilder {
-    pub fn write_u64(&mut self, number: u64) -> Result<(), String> {
+    pub fn write_u64(&mut self, number: u64) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_i64(&mut self, number: i64) -> Result<(), String> {
+    pub fn write_i64(&mut self, number: i64) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_u32(&mut self, number: u32) -> Result<(), String> {
+    pub fn write_u32(&mut self, number: u32) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_i32(&mut self, number: i32) -> Result<(), String> {
+    pub fn write_i32(&mut self, number: i32) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_u16(&mut self, number: u16) -> Result<(), String> {
+    pub fn write_u16(&mut self, number: u16) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_i16(&mut self, number: i16) -> Result<(), String> {
+    pub fn write_i16(&mut self, number: i16){
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_u8(&mut self, number: u8) -> Result<(), String> {
+    pub fn write_u8(&mut self, number: u8) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_i8(&mut self, number: i8) -> Result<(), String> {
+    pub fn write_i8(&mut self, number: i8) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_usize(&mut self, number: usize) -> Result<(), String> {
+    pub fn write_usize(&mut self, number: usize) {
         for byte in (number as u32).to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_f32(&mut self, number: f32) -> Result<(), String> {
+    pub fn write_f32(&mut self, number: f32) {
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
-    pub fn write_bool(&mut self, boolean: bool) -> Result<(), String> {
+    pub fn write_bool(&mut self, boolean: bool) {
         let number: u8 = if boolean {1} else {0};
         for byte in number.to_le_bytes() {
             self.raw_data.push(byte);
         }
-        Ok(())
     }
     pub fn write_literal_string(&mut self, string: &str) -> Result<(), String> {
         // write an ascii string to the data
@@ -92,11 +81,10 @@ impl ChunkBuilder {
         let string = string.resolve(&strings)?;
         self.write_literal_string(string)
     }
-    pub fn write_bytes(&mut self, data: &[u8]) -> Result<(), String> {
+    pub fn write_bytes(&mut self, data: &[u8]) {
         for byte in data {
             self.raw_data.push(*byte);
         }
-        Ok(())
     }
     pub fn overwrite_data(&mut self, data: &[u8], position: usize) -> Result<(), String> {
         if position + data.len() >= self.len() {
--- src/serialize/embedded_textures.rs
+++ src/serialize/embedded_textures.rs
@@ -3,10 +3,10 @@ use crate::serialize::all::{DataBuilder, GMRef};
 use crate::serialize::chunk_writing::ChunkBuilder;
 
 
-pub fn build_chunk_TXTR(data_builder: &mut DataBuilder, gm_data: &GMData, texture_pages: Vec<image::DynamicImage>) -> Result<(), String> {
+pub fn build_chunk_txtr(data_builder: &mut DataBuilder, gm_data: &GMData, texture_pages: Vec<image::DynamicImage>) -> Result<(), String> {
     let mut builder: ChunkBuilder = ChunkBuilder { raw_data: Vec::new(), chunk_name: "TXTR", abs_pos: data_builder.len() };
     let len: usize = texture_pages.len();
-    builder.write_usize(len)?;
+    builder.write_usize(len);
 
     for i in 0..len {
         data_builder.push_pointer_position(&mut builder, GMRef::TexturePage(i))?;
@@ -14,17 +14,17 @@ pub fn build_chunk_TXTR(data_builder: &mut DataBuilder, gm_data: &GMData, textur
 
     for (i, texture_page) in texture_pages.iter().enumerate() {
         data_builder.push_pointing_to(&mut builder, GMRef::TexturePage(i))?;
-        builder.write_u32(0)?;          // scaled
+        builder.write_u32(0);          // scaled
         if gm_data.general_info.is_version_at_least(2, 0, 6, 0) {
-            builder.write_u32(0)?;      // number of generated mipmap levels
+            builder.write_u32(0);      // number of generated mipmap levels
         }
         if gm_data.general_info.is_version_at_least(2022, 3, 0, 0) {
-            builder.write_u32(0)?;      // texture block size  (this placeholder will definitely break)
+            builder.write_u32(0);      // texture block size  (this placeholder will definitely break)
         }
         if gm_data.general_info.is_version_at_least(2022, 9, 0, 0) {
-            builder.write_u32(texture_page.width())?;
-            builder.write_u32(texture_page.height())?;
-            builder.write_usize(i)?;
+            builder.write_u32(texture_page.width());
+            builder.write_u32(texture_page.height());
+            builder.write_usize(i);
         }
         data_builder.push_pointer_position(&mut builder, GMRef::TexturePageData(i))?;
     }
@@ -35,7 +35,7 @@ pub fn build_chunk_TXTR(data_builder: &mut DataBuilder, gm_data: &GMData, textur
         if let Err(error) = texture_page.write_to(&mut buf, image::ImageFormat::Png) {
             return Err(format!("Could not build PNG image for texture page with index {}: {}", i, error))
         }
-        builder.write_bytes(&buf.into_inner())?;
+        builder.write_bytes(&buf.into_inner());
     }
 
     Ok(())
