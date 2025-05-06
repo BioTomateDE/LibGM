e56b181d72baeecb2e7b135469b6e3adb9bc996b
biotomatede <latus.kati@gmail.com>
2025-05-06 18:56:44 +0200

    function refactor

--- src/deserialize/functions.rs
+++ src/deserialize/functions.rs
@@ -28,20 +28,28 @@ pub struct GMCodeLocal {
 
 pub fn parse_chunk_func(chunk: &mut GMChunk, strings: &GMStrings, chunk_code: &GMChunk) -> Result<(GMFunctions, Vec<GMCodeLocal>), String> {
     chunk.cur_pos = 0;
-    let functions_length: usize = chunk.read_usize()?;
-    let mut functions_by_index: Vec<GMFunction> = Vec::with_capacity(functions_length);
+    let functions_count: usize = chunk.read_usize()?;
+    let mut functions_by_index: Vec<GMFunction> = Vec::with_capacity(functions_count);
     let mut occurrences_to_refs: HashMap<usize, GMRef<GMFunction>> = HashMap::new();
 
-    for i in 0..functions_length {
-        let function_name: GMRef<String> = chunk.read_gm_string(strings)?;
+    for i in 0..functions_count {
+        let name: GMRef<String> = chunk.read_gm_string(strings)?;
         let occurrence_count: usize = chunk.read_usize()?;
-        let first_occurrence: i32 = chunk.read_i32()? - chunk_code.abs_pos as i32;
-        let occurrences: Vec<usize> = get_occurrences(occurrence_count, first_occurrence, chunk_code);
+        let first_occurrence: i32 = chunk.read_i32()?;
+        let occurrences: Vec<usize> = parse_occurrence_chain(chunk_code, name.resolve(&strings.strings_by_index)?, first_occurrence, occurrence_count)?;
+
         for occurrence in &occurrences {
-            occurrences_to_refs.insert(*occurrence, GMRef::new(i));
+            if let Some(old_value) = occurrences_to_refs.insert(*occurrence, GMRef::new(i)) {
+                return Err(format!(
+                    "Conflicting occurrence positions while parsing functions: absolute position {} \
+                    was already set for function #{} with name \"{}\"; trying to set to function #{} with name \"{}\".",
+                    occurrence, old_value.index, old_value.resolve(&functions_by_index)?.name.display(strings), i, name.display(strings),
+                ))
+            }
         }
+
         let function: GMFunction = GMFunction {
-            name: function_name,
+            name,
             occurrences,
         };
         functions_by_index.push(function);
@@ -88,28 +96,38 @@ pub fn parse_chunk_func(chunk: &mut GMChunk, strings: &GMStrings, chunk_code: &G
 }
 
 
-fn get_occurrences(count: usize, first_occurrence: i32, chunk_code: &GMChunk) -> Vec<usize> {
-    let mut occurrences: Vec<usize> = Vec::new();
-    let mut occurrence: i32 = first_occurrence + 4;
+fn parse_occurrence_chain(chunk_code: &GMChunk, function_name: &str, first_occurrence_abs_pos: i32, occurrence_count: usize) -> Result<Vec<usize>, String> {
+    if occurrence_count < 1 {
+        return Ok(vec![]);
+    }
 
-    // let mut i = 0;
+    let mut occurrence_pos: i32 = first_occurrence_abs_pos - chunk_code.abs_pos as i32 + 4;
+    let mut occurrence_pos: usize = occurrence_pos.try_into()
+        .map_err(|_| format!(
+            "First occurrence of function \"{}\" is out of bounds; should be: {} <= {} < {}.",
+            function_name, chunk_code.abs_pos, first_occurrence_abs_pos, chunk_code.abs_pos + chunk_code.data.len(),
+        ))?;
 
-    for _ in 0..count {
-        // println!("occ {occurrence} | {}", chunk_CODE.abs_pos);
-        // occurrence -= chunk_CODE.abs_pos as i32;
-        occurrences.push(occurrence as usize - 4);
-        // TODO index safety
-        let raw: [u8; 4] = chunk_code.data[(occurrence as usize) .. (occurrence as usize)+4].try_into().unwrap();
+    let mut occurrences: Vec<usize> = Vec::with_capacity(occurrence_count);
+
+    for _ in 0..occurrence_count {
+        occurrences.push(occurrence_pos);
+        let raw: [u8; 4] = chunk_code.data.get(occurrence_pos .. occurrence_pos+4)
+            .ok_or_else(|| format!("Trying to read next occurrence offset out of bounds \
+            while parsing function reference chain: {} > {}", occurrence_pos, chunk_code.data.len()))?
+            .try_into().unwrap();
+
         // println!("{}", hexdump(&chunk_CODE.data, (occurrence as usize)-4, Some(occurrence as usize+4)).unwrap());
-        if chunk_code.data[occurrence as usize - 1] != 0xD9 {
+        if chunk_code.data[occurrence_pos- 1] != 0xD9 {
+            log::info!("funciton not d9: {occurrence_pos} {}", chunk_code.data[occurrence_pos - 1]);
             break;
         }
-        occurrence += i32::from_le_bytes(raw);
-        // i += 1;
+        occurrence_pos += i32::from_le_bytes(raw) as usize;
+        // log::info!("functionref {} {} {} {}", function_name, occurrence_count, occurrence_pos, i32::from_le_bytes(raw))
     }
 
     // println!("FUNCITONSBDF | expected: {count:<10},  actual: {i:<10}   {}", count==i);
 
-    occurrences
+    Ok(occurrences)
 }
 
