02682fc87130c61573a6f00ad3f180303372aa25
BioTomateDE <latus.kati@gmail.com>
2025-05-06 19:11:20 +0200

    gf

--- src/serialize/variables.rs
+++ src/serialize/variables.rs
@@ -1,4 +1,8 @@
 use crate::deserialize::all::GMData;
+use crate::deserialize::chunk_reading::GMRef;
+use crate::deserialize::code::GMInstanceType;
+use crate::deserialize::general_info::GMGeneralInfo;
+use crate::deserialize::variables::GMVariable;
 use crate::serialize::all::{build_chunk, DataBuilder};
 use crate::serialize::chunk_writing::{ChunkBuilder, GMPointer};
 
@@ -14,11 +18,40 @@ pub fn build_chunk_vari(data_builder: &mut DataBuilder, gm_data: &GMData) -> Res
 
     for (i, variable) in gm_data.variables.variables.iter().enumerate() {
         data_builder.push_pointer_resolve(&mut builder, GMPointer::variable(i))?;
-        // TODO
+        build_variable(data_builder, &mut builder, &gm_data.general_info, variable)
+            .map_err(|e| format!("{e} for variable #{i} with name {}", variable.name.display(&gm_data.strings)))?;
     }
 
 
     build_chunk(data_builder, builder)?;
     Ok(())
+}
+
+fn build_variable(data_builder: &mut DataBuilder, builder: &mut ChunkBuilder, general_info: &GMGeneralInfo, variable: &GMVariable) -> Result<(), String> {
+    builder.write_gm_string(data_builder, &variable.name)?;
+    if general_info.bytecode_version >= 15 {
+        builder.write_i32(build_instance_type(&variable.instance_type));
+        builder.write_i32(variable.variable_id.ok_or("Variable ID not set")?);
+    }
+    // TODO: go through code and find occurrences idk
+
+    Ok(())
+}
+
+
+pub fn build_instance_type(instance_type: &GMInstanceType) -> i32 {
+    match instance_type {
+        GMInstanceType::Undefined => 0,
+        GMInstanceType::Self_(Some(obj_ref)) => obj_ref.index as i32,
+        GMInstanceType::Self_(None) => -1,
+        GMInstanceType::Other => -2,
+        GMInstanceType::All => -3,
+        GMInstanceType::Noone => -4,
+        GMInstanceType::Global => -5,
+        GMInstanceType::Local => -7,
+        GMInstanceType::Stacktop => -9,
+        GMInstanceType::Arg => -15,
+        GMInstanceType::Static => -16,
+    }
 }
 
