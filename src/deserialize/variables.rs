dda111748f6095934c08b68986f542e3236f67aa
BioTomateDE <latus.kati@gmail.com>
2025-05-06 17:48:47 +0200

    vari more fix

--- src/deserialize/variables.rs
+++ src/deserialize/variables.rs
@@ -1,7 +1,7 @@
 ﻿use std::collections::HashMap;
 use itertools::izip;
 use crate::deserialize::chunk_reading::{GMChunk, GMRef};
-use crate::deserialize::code::{parse_instance_type, GMCodeVariable, GMDataType, GMInstanceType, GMOpcode, GMPopInstruction, GMVariableType};
+use crate::deserialize::code::{parse_instance_type, GMInstanceType};
 use crate::deserialize::general_info::GMGeneralInfo;
 use crate::deserialize::strings::GMStrings;
 
@@ -14,15 +14,11 @@ pub struct GMVariable {
 
 #[derive(Debug, Clone)]
 pub struct GMVariables {
-    pub global_variables: Vec<GMVariable>,
-    pub instance_variables: Vec<GMVariable>,
-    pub local_variables: Vec<GMVariable>,
-
-    // Maps absolute positions of variable occurrences (in code) to variable references.
-    // Only meant for parsing code; irrelevant (and potentially incorrect) after parsing.
-    pub global_occurrence_map: HashMap<usize, GMRef<GMVariable>>,
-    pub instance_occurrence_map: HashMap<usize, GMRef<GMVariable>>,
-    pub local_occurrence_map: HashMap<usize, GMRef<GMVariable>>,
+    /// List of all variables; mixing global, local and self.
+    pub variables: Vec<GMVariable>,
+    /// Maps absolute positions of variable occurrences (in code) to variable references.
+    /// Only meant for parsing code; irrelevant (and potentially incorrect) after parsing.
+    pub occurrence_map: HashMap<usize, GMRef<GMVariable>>,
 }
 
 pub fn parse_chunk_vari(chunk: &mut GMChunk, strings: &GMStrings, general_info: &GMGeneralInfo, chunk_code: &mut GMChunk) -> Result<GMVariables, String> {
@@ -31,21 +27,15 @@ pub fn parse_chunk_vari(chunk: &mut GMChunk, strings: &GMStrings, general_info: 
     let instances_count: usize = chunk.read_usize()?;       // the amount of `Self` variables (local to own object)
     let locals_count: usize = chunk.read_usize()?;          // the amount of `Local` variables (local to own code script)
 
-    let mut global_variables: Vec<GMVariable> = Vec::with_capacity(globals_count);
-    let mut instance_variables: Vec<GMVariable> = Vec::with_capacity(instances_count);
-    let mut local_variables: Vec<GMVariable> = Vec::with_capacity(locals_count);
+    let mut variables: Vec<GMVariable> = Vec::with_capacity(globals_count);
+    let mut occurrence_map: HashMap<usize, GMRef<GMVariable>> = HashMap::new();
+    let mut cur_index: usize = 0;
 
-    let mut global_occurrence_map: HashMap<usize, GMRef<GMVariable>> = HashMap::new();
-    let mut instance_occurrence_map: HashMap<usize, GMRef<GMVariable>> = HashMap::new();
-    let mut local_occurrence_map: HashMap<usize, GMRef<GMVariable>> = HashMap::new();
-
-    for (variable_count, variables, occurrence_map, default_instance_type) in izip!(
+    for (variable_count, default_instance_type) in izip!(
         [globals_count, instances_count, locals_count],
-        [&mut global_variables, &mut instance_variables, &mut local_variables],
-        [&mut global_occurrence_map, &mut instance_occurrence_map, &mut local_occurrence_map],
         [GMInstanceType::Global, GMInstanceType::Self_(None), GMInstanceType::Local],
     ) {
-        for i in 0..variable_count {
+        for _ in 0..variable_count {
             let name: GMRef<String> = chunk.read_gm_string(strings)?;
 
             // bytecode>=15 might reads instance type here, so maybe it doesn't have the 3 global/instance/local count in the beginning?
@@ -60,6 +50,7 @@ pub fn parse_chunk_vari(chunk: &mut GMChunk, strings: &GMStrings, general_info: 
             let occurrences_count: usize = chunk.read_usize()?;
             let first_occurrence_address: i32 = chunk.read_i32()?;
 
+            // log::info!("getting {} occurrences of {:?} variable {}", occurrences_count, instance_type, name.display(strings));
             let occurrences: Vec<usize> = parse_occurrence_chain(
                 chunk_code,
                 name.display(strings),
@@ -67,12 +58,19 @@ pub fn parse_chunk_vari(chunk: &mut GMChunk, strings: &GMStrings, general_info: 
                 occurrences_count
             )?;
 
+            // let occurrence_map = match instance_type {
+            //     GMInstanceType::Global => &mut global_occurrence_map,
+            //     GMInstanceType::Self_(_) => &mut instance_occurrence_map,
+            //     GMInstanceType::Local => &mut local_occurrence_map,
+            //     other => return Err(format!("Invalid instance type {other:?} for variable {} while parsing variables.", name.display(strings))),
+            // };
+
             for occurrence in occurrences {
-                if let Some(old_value) = occurrence_map.insert(occurrence, GMRef::new(i)) {
+                if let Some(old_value) = occurrence_map.insert(occurrence, GMRef::new(cur_index)) {
                     return Err(format!(
                         "Conflicting occurrence positions while parsing variables: absolute position {} \
                         was already set for {:?} variable #{} with name \"{}\"; trying to set to variable #{} with name \"{}\".",
-                        occurrence, default_instance_type, old_value.index, old_value.resolve(variables)?.name.display(strings), i, name.display(strings),
+                        occurrence, default_instance_type, old_value.index, old_value.resolve(&variables)?.name.display(strings), cur_index, name.display(strings),
                     ))
                 }
             }
@@ -82,17 +80,15 @@ pub fn parse_chunk_vari(chunk: &mut GMChunk, strings: &GMStrings, general_info: 
                 instance_type,
                 variable_id,
             });
+            cur_index += 1;
         }
     }
 
     // log::debug!("var len: {}", variables_by_index.len());
+
     Ok(GMVariables {
-        global_variables,
-        instance_variables,
-        local_variables,
-        global_occurrence_map,
-        instance_occurrence_map,
-        local_occurrence_map,
+        variables,
+        occurrence_map,
     })
 }
 
@@ -107,7 +103,7 @@ fn parse_occurrence_chain(
         return Ok(vec![]);
     }
 
-    let occurrence_pos: i32 = first_occurrence_abs_pos - chunk_code.abs_pos as i32;
+    let occurrence_pos: i32 = first_occurrence_abs_pos - chunk_code.abs_pos as i32 + 4;
     let mut occurrence_pos: usize = occurrence_pos.try_into()
         .map_err(|_| format!(
             "First occurrence of variable \"{}\" is out of bounds; should be: {} <= {} < {}.",
@@ -116,21 +112,24 @@ fn parse_occurrence_chain(
 
     let mut occurrences: Vec<usize> = Vec::with_capacity(occurrence_count);
 
-    for _ in 0..occurrence_count-1 {        // this -1 seems to be correct (?)
-        occurrence_pos += 4;
+    for _ in 0..occurrence_count {
+        occurrences.push(occurrence_pos);
         chunk_code.cur_pos = occurrence_pos;
         let offset: usize = read_variable_reference(chunk_code)?;
         occurrence_pos += offset;
-        occurrences.push(occurrence_pos);
     }
+
+    // occurrence_pos now represents "name string id" {~~}
 
     Ok(occurrences)
 }
 
 
-pub fn read_variable_reference(chunk: &mut GMChunk) -> Result<usize, String> {
-    let raw_value: i32 = chunk.read_i32()?;
+pub fn read_variable_reference(chunk_code: &mut GMChunk) -> Result<usize, String> {
+    // log::debug!("{} | {}", chunk_code.cur_pos, crate::printing::hexdump(chunk_code.data, chunk_code.cur_pos-8, Some(chunk_code.cur_pos+8))?);
+    let raw_value: i32 = chunk_code.read_i32()?;
     let next_occurrence_offset: i32 = raw_value & 0x07FFFFFF;
+    // log::info!("b {next_occurrence_offset}");
     let next_occurrence_offset: usize = next_occurrence_offset as usize;
     Ok(next_occurrence_offset)
 }
