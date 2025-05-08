f041e7e5c75144ace1521db532234c3f6a0b2dce
BioTomateDE <latus.kati@gmail.com>
2025-05-08 13:43:07 +0200

    modding basic

--- src/export_mod/strings.rs
+++ src/export_mod/strings.rs
@@ -1,34 +1,12 @@
-use std::collections::HashMap;
-use std::io::Write;
-use serde::Serialize;
-use zip::write::{FileOptions, SimpleFileOptions};
 use crate::deserialize::all::GMData;
-use crate::export_mod::export::{ModWriter, FILE_OPTIONS};
+use crate::export_mod::unordered_list::export_changes_unordered_list;
 
-
-#[derive(Debug, Clone, Serialize)]
-struct ModStrings<'a> {
-    add: &'a Vec<&'a String>,
-    edit: &'a HashMap<usize, &'a String>,
+pub fn export_strings(original_data: &GMData, modified_data: &GMData) -> Result<serde_json::Value, String> {
+    export_changes_unordered_list(
+        &original_data.strings.strings_by_index,
+        &modified_data.strings.strings_by_index,
+        |string| string.clone(),
+        |_original_string, modified_string| modified_string.clone(),
+    )
 }
 
-// fn export_strings(zip_writer: &mut ModWriter, data_orig: &GMData, data_mod: &GMData) -> Result<(), String> {
-//     let add: Vec<&String> = Vec::new();
-//     for string in data_orig.strings.strings_by_index {
-//         w
-//     }
-//
-//     let mod_strings = ModStrings { add: &vec![], edit: Default::default() }
-//
-//     zip_writer.start_file("strings.txt", *FILE_OPTIONS)
-//         .map_err(|e| format!("Failed to create strings file: {e}"))?;
-//
-//     let string: String = serde_json::to_string(mod_strings)
-//         .map_err(|e| format!("Failed to serialize strings: {e}"))?;
-//
-//     zip_writer.write_all(string.as_bytes())
-//         .map_err(|e| format!("Failed to write strings file: {e}"))?;
-//
-//     Ok(())
-// }
-//
