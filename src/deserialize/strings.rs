c871d670f7ee98e1568fa064a03ecb86ccc05732
biotomatede <latus.kati@gmail.com>
2025-05-12 21:43:40 +0200

    fixed se string actually

--- src/deserialize/strings.rs
+++ src/deserialize/strings.rs
@@ -12,20 +12,23 @@ pub struct GMStrings {
 pub fn parse_chunk_strg(chunk: &mut GMChunk) -> Result<GMStrings, String> {
     chunk.cur_pos = 0;
     let string_count: usize = chunk.read_usize()?;
-    let mut start_positions: Vec<usize> = Vec::with_capacity(string_count);
+    let mut abs_start_positions: Vec<usize> = Vec::with_capacity(string_count);
     for _ in 0..string_count {
-        start_positions.push(chunk.read_usize()?);
+        abs_start_positions.push(chunk.read_usize()?);
     }
 
     let mut strings_by_index: Vec<String> = Vec::with_capacity(string_count);
     let mut abs_pos_to_reference: HashMap<usize, GMRef<String>> = HashMap::new();
 
-    for (i, start_position) in start_positions.iter().enumerate() {
-        chunk.cur_pos = *start_position - chunk.abs_pos;
+    for (i, abs_start_position) in abs_start_positions.iter().enumerate() {
+        chunk.cur_pos = *abs_start_position - chunk.abs_pos;
+        log::info!("afasfax {} {} {}", i, abs_start_position, chunk.cur_pos);
         let string_length: usize = chunk.read_usize()?;
         let string: String = chunk.read_literal_string(string_length)?;
         strings_by_index.push(string.clone());
-        abs_pos_to_reference.insert(start_position + 4, GMRef::new(i));
+        // start_position + 4 because gamemaker moment
+        // gamemaker does this because it's faster to access strings if you don't need to add or subtract 4 every time
+        abs_pos_to_reference.insert(*abs_start_position + 4, GMRef::new(i));
     }
 
     Ok(GMStrings {
