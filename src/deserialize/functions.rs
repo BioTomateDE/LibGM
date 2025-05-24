53b0df3cb927ae94d7d01ea6c754f4bd95593da0
BioTomateDE <biotomatede@proton.me>
2025-05-24 14:21:22 +0200

    se functions code locals fix and de functions stable version

--- src/deserialize/functions.rs
+++ src/deserialize/functions.rs
@@ -1,6 +1,7 @@
 ﻿use std::collections::HashMap;
 use crate::deserialize::chunk_reading::{GMChunk, GMRef};
 use crate::deserialize::code::parse_occurrence_chain;
+use crate::deserialize::general_info::GMGeneralInfo;
 use crate::deserialize::strings::GMStrings;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -29,17 +30,25 @@ pub struct GMCodeLocal {
     pub variables: Vec<GMCodeLocalVariable>,
 }
 
-pub fn parse_chunk_func(chunk: &mut GMChunk, strings: &GMStrings, chunk_code: &mut GMChunk) -> Result<(GMFunctions, Vec<GMCodeLocal>), String> {
+pub fn parse_chunk_func(chunk: &mut GMChunk, general_info: &GMGeneralInfo, strings: &GMStrings, chunk_code: &mut GMChunk) -> Result<(GMFunctions, Vec<GMCodeLocal>), String> {
     chunk.cur_pos = 0;
-    let functions_count: usize = chunk.read_usize()?;
+
+    // read functions
+    let functions_count: usize = if general_info.bytecode_version <= 14 {
+        chunk.data.len() / 12
+    } else {
+        chunk.read_usize()?
+    };
+    
     let mut functions_by_index: Vec<GMFunction> = Vec::with_capacity(functions_count);
     let mut occurrences_to_refs: HashMap<usize, GMRef<GMFunction>> = HashMap::new();
 
     for i in 0..functions_count {
         let name: GMRef<String> = chunk.read_gm_string(strings)?;
         let occurrence_count: usize = chunk.read_usize()?;
-        let first_occurrence: i32 = chunk.read_i32()?;
-        let (occurrences, name_string_id): (Vec<usize>, i32) = parse_occurrence_chain(chunk_code, name.resolve(&strings.strings_by_index)?, first_occurrence, occurrence_count)?;
+        let first_occurrence_abs_pos: i32 = chunk.read_i32()?;
+
+        let (occurrences, name_string_id): (Vec<usize>, i32) = parse_occurrence_chain(chunk_code, name.resolve(&strings.strings_by_index)?, first_occurrence_abs_pos, occurrence_count)?;
 
         for occurrence in &occurrences {
             if let Some(old_value) = occurrences_to_refs.insert(*occurrence, GMRef::new(i)) {
@@ -62,31 +71,40 @@ pub fn parse_chunk_func(chunk: &mut GMChunk, strings: &GMStrings, chunk_code: &m
         occurrences_to_refs,
     };
 
-    let code_locals_length: usize = chunk.read_usize()?;
-    let mut code_locals: Vec<GMCodeLocal> = Vec::with_capacity(code_locals_length);
-
-    for _ in 0..code_locals_length {
-        let local_variables_count: usize = chunk.read_usize()?;
-        let name: GMRef<String> = chunk.read_gm_string(&strings)?;
-        let mut variables: Vec<GMCodeLocalVariable> = Vec::with_capacity(local_variables_count);
-
-        for _ in 0..local_variables_count {
-            let variable_index: usize = chunk.read_usize()?;
-            let variable_name: GMRef<String> = chunk.read_gm_string(&strings)?;
-            let variable: GMCodeLocalVariable = GMCodeLocalVariable {
-                index: variable_index,
-                name: variable_name,
-            };
-            variables.push(variable);
-        }
-
-        let code_local: GMCodeLocal = GMCodeLocal {
-            name,
-            variables,
-        };
-        code_locals.push(code_local);
-
+    // read code locals
+    let code_locals_count: usize = if general_info.bytecode_version <= 14 || general_info.is_version_at_least(2024, 8, 0, 0) {
+        0
+    } else {
+        chunk.read_usize()?
+    };
+    
+    let mut code_locals: Vec<GMCodeLocal> = Vec::with_capacity(code_locals_count);
+    for _ in 0..code_locals_count {
+        code_locals.push(read_code_local(chunk, strings)?);
     }
 
     Ok((functions, code_locals))
 }
+
+
+fn read_code_local(chunk: &mut GMChunk, strings: &GMStrings) -> Result<GMCodeLocal, String> {
+    let local_variables_count: usize = chunk.read_usize()?;
+    let name: GMRef<String> = chunk.read_gm_string(&strings)?;
+    let mut variables: Vec<GMCodeLocalVariable> = Vec::with_capacity(local_variables_count);
+
+    for _ in 0..local_variables_count {
+        let variable_index: usize = chunk.read_usize()?;
+        let variable_name: GMRef<String> = chunk.read_gm_string(&strings)?;
+        let variable: GMCodeLocalVariable = GMCodeLocalVariable {
+            index: variable_index,
+            name: variable_name,
+        };
+        variables.push(variable);
+    }
+
+    Ok(GMCodeLocal {
+        name,
+        variables,
+    })
+}
+
--- src/serialize/functions.rs
+++ src/serialize/functions.rs
@@ -1,14 +1,17 @@
 use std::collections::HashMap;
 use crate::deserialize::all::GMData;
+use crate::deserialize::functions::GMCodeLocal;
 use crate::serialize::all::DataBuilder;
-use crate::serialize::chunk_writing::{ChunkBuilder, GMPointer};
+use crate::serialize::chunk_writing::ChunkBuilder;
 
 pub fn build_chunk_func(data_builder: &mut DataBuilder, gm_data: &GMData, function_occurrences_map: HashMap<usize, Vec<usize>>) -> Result<(), String> {
     let mut builder = ChunkBuilder::new(data_builder, "FUNC");
     
     // write functions
-    builder.write_usize(gm_data.functions.functions_by_index.len());
-
+    if gm_data.general_info.bytecode_version >= 14 {
+        builder.write_usize(gm_data.functions.functions_by_index.len());
+    }
+    
     for (i, function) in gm_data.functions.functions_by_index.iter().enumerate() {
         // there is no pointer list
         builder.write_gm_string(data_builder, &function.name)?;
@@ -23,24 +26,27 @@ pub fn build_chunk_func(data_builder: &mut DataBuilder, gm_data: &GMData, functi
     }
     
     // write code locals
-    builder.write_usize(gm_data.code_locals.len());
-
-    for i in 0..gm_data.code_locals.len() {
-        data_builder.write_pointer_placeholder(&mut builder, GMPointer::CodeLocal(i))?;
-    }
-
-    for (i, code_local) in gm_data.code_locals.iter().enumerate() {
-        data_builder.resolve_pointer(&mut builder, GMPointer::CodeLocal(i))?;
-        builder.write_gm_string(data_builder, &code_local.name)?;
-        builder.write_usize(code_local.variables.len());
-        
-        for variable in &code_local.variables {
-            builder.write_usize(variable.index);
-            builder.write_gm_string(data_builder, &variable.name)?;
+    if !gm_data.general_info.is_version_at_least(2024, 8, 0, 0) {
+        builder.write_usize(gm_data.code_locals.len());
+        for code_local in &gm_data.code_locals {
+            build_code_local(data_builder, &mut builder, code_local)?;
         }
     }
 
     builder.finish(data_builder)?;
+    Ok(())
+}
+
+
+fn build_code_local(data_builder: &mut DataBuilder, builder: &mut ChunkBuilder, code_local: &GMCodeLocal) -> Result<(), String> {
+    builder.write_gm_string(data_builder, &code_local.name)?;
+    builder.write_usize(code_local.variables.len());
+
+    for variable in &code_local.variables {
+        builder.write_usize(variable.index);
+        builder.write_gm_string(data_builder, &variable.name)?;
+    }
+    
     Ok(())
 }
 
