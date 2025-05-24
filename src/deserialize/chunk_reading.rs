8e8316c9c7c27249fbba44b3a6b8b6deadb09149
BioTomateDE <biotomatede@proton.me>
2025-05-24 13:32:49 +0200

    remove debuglogs

--- src/deserialize/chunk_reading.rs
+++ src/deserialize/chunk_reading.rs
@@ -1,5 +1,4 @@
-﻿use itertools::chain;
-use crate::deserialize::strings::GMStrings;
+﻿use crate::deserialize::strings::GMStrings;
 
 // GMRef is for parsing chunks:
 // It has (fake) generic types to make it clearer
@@ -252,7 +251,6 @@ impl GMChunk<'_> {
     /// read pointer to pointer list (only used in rooms)
     pub fn read_pointer_list(&mut self) -> Result<Vec<usize>, String> {
         let abs_pointers_start_pos: usize = self.read_usize()?;
-        log::info!("hfskkfhernjkfdsnhsfgighfrsw {}  {} // {}", abs_pointers_start_pos, self.abs_pos, self.cur_pos as i64 - abs_pointers_start_pos as i64 +self.abs_pos as i64);
         let pointers_start_pos: usize = abs_pointers_start_pos.checked_sub(self.abs_pos).ok_or_else(|| format!(
             "Pointer to start of Pointer list underflowed at position {} in chunk '{}': {} - {} < 0",
             self.cur_pos - 4, self.name, abs_pointers_start_pos, self.abs_pos,
