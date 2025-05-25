ec4a96b767f392283392bfa349f7988835cc815b
BioTomateDE <latuskati@gmail.com>
2025-05-25 15:10:54 +0200

    more object fix

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
     let logger = biologischer_log::init_logger(env!("CARGO_PKG_NAME"));
 
     let args: Vec<String> = std::env::args().collect();
-    let original_data_file_path: &Path = Path::new(args.get(1).map_or("data.win", |s| s));
+    let original_data_file_path: &Path = Path::new(args.get(1).map_or("data_out.win", |s| s));
 
     info!("Loading data file \"{}\"", original_data_file_path.display());
     let original_data: Vec<u8> = match read_data_file(original_data_file_path) {
