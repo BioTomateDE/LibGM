0e6cb16d98488714536d774dc21eac5ad8083ae9
BioTomateDE <biotomatede@proton.me>
2025-05-25 12:43:08 +0200

    benchmarking ms

--- src/debug_utils.rs
+++ src/debug_utils.rs
@@ -3,7 +3,7 @@ macro_rules! trace_build {
     ($label:expr, $expr:expr) => {{
         let _start = ::cpu_time::ProcessTime::now();
         let result = $expr;
-        ::log::trace!("Building chunk '{}' took {:.2?}", $label, _start.elapsed());
+        ::log::trace!("Building chunk '{}' took {}", $label, _start.elapsed().ms());
         result
     }};
 }
@@ -13,8 +13,19 @@ macro_rules! trace_parse {
     ($label:expr, $expr:expr) => {{
         let _start = ::cpu_time::ProcessTime::now();
         let result = $expr;
-        ::log::trace!("Parsing chunk '{}' took {:.2?}", $label, _start.elapsed());
+        ::log::trace!("Parsing chunk '{}' took {}", $label, _start.elapsed().ms());
         result
     }};
 }
 
+
+
+pub trait DurationExt {
+    fn ms(&self) -> String;
+}
+
+impl DurationExt for std::time::Duration {
+    fn ms(&self) -> String {
+        format!("{:.2} ms", self.as_secs_f64() * 1000.0)
+    }
+}
