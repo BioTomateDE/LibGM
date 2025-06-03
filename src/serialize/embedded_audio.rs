5d04d038bf14437de75475cb0e110e1aac629b89
BioTomateDE <latus.kati@gmail.com>
2025-06-03 14:58:08 +0200

    ITS WORKIGN

--- src/serialize/embedded_audio.rs
+++ src/serialize/embedded_audio.rs
@@ -1,5 +1,4 @@
 use crate::deserialize::all::GMData;
-use crate::deserialize::embedded_audio::GMEmbeddedAudio;
 use crate::serialize::chunk_writing::{DataBuilder, GMPointer};
 
 pub fn build_chunk_audo(builder: &mut DataBuilder, gm_data: &GMData) -> Result<(), String> {
@@ -11,11 +10,15 @@ pub fn build_chunk_audo(builder: &mut DataBuilder, gm_data: &GMData) -> Result<(
         builder.write_placeholder(GMPointer::Audio(i))?;
     }
 
-    for i in 0..len {
+    for (i, audio) in gm_data.audios.audios_by_index.iter().enumerate() {
         builder.resolve_pointer(GMPointer::Audio(i))?;
-        let audio: &GMEmbeddedAudio = &gm_data.audios.audios_by_index[i];
         builder.write_usize(audio.raw_data.len());
         builder.write_bytes(&audio.raw_data);
+
+        // padding
+        if i + 1 != len {
+            builder.align(4, 0x00);
+        }
     }
 
     builder.finish_chunk(&gm_data.general_info)?;
