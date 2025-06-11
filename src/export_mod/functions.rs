55a7931d1e546685274e2234a77e4f46fbf04aca
BioTomateDE <biotomatede@proton.me>
2025-06-12 01:06:02 +0200

    rdfg

--- src/export_mod/functions.rs
+++ src/export_mod/functions.rs
@@ -2,38 +2,38 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use crate::deserialize::functions::GMFunction;
 use crate::export_mod::export::{edit_field, GModData, ModUnorderedRef};
-use crate::export_mod::unordered_list::{ AModUnorderedListChanges, GModUnorderedListChanges};
+use crate::export_mod::unordered_list::{EditUnorderedList, GModUnorderedListChanges};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ModFunction {
+pub struct AddFunction {
+    pub name: ModUnorderedRef,  // String
+    // idk how to handle function and variable occurrences yet
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EditFunction {
     pub name: Option<ModUnorderedRef>,  // String
     // idk how to handle function and variable occurrences yet
 }
 
 impl GModData<'_, '_> {
-    pub fn convert_functions_additions(&self, gm_functions: &Vec<GMFunction>) -> Result<Vec<ModFunction>, String> {
-        let mut mod_functions: Vec<ModFunction> = Vec::with_capacity(gm_functions.len());
-
-        for function in gm_functions {
-            mod_functions.push(ModFunction {
-                name: Some(self.resolve_string_ref(&function.name)?),
-            });
-        }
-
-        Ok(mod_functions)
+    fn convert_functions_additions(&self, functions: &[GMFunction]) -> Result<Vec<AddFunction>, String> {
+        functions.iter().map(|i| {
+            Ok(AddFunction {
+                name: self.resolve_string_ref(&i.name)?,
+            })
+        }).collect()
     }
 
-    pub fn convert_functions(&self, changes: &GModUnorderedListChanges<GMFunction>) -> Result<AModUnorderedListChanges<ModFunction>, String> {
-        let additions: Vec<ModFunction> = self.convert_functions_additions(&changes.additions)?;
-        let mut edits: HashMap<usize, ModFunction> = HashMap::new();
-
-        for (index, (original, modified)) in &changes.edits {
-            edits.insert(*index, ModFunction {
-                name: edit_field(&self.resolve_string_ref(&original.name)?, &self.resolve_string_ref(&modified.name)?),
-            });
-        }
-
-        Ok(AModUnorderedListChanges { additions, edits })
+    pub fn convert_functions(&self, changes: &GModUnorderedListChanges<GMFunction>) -> Result<EditUnorderedList<AddFunction, EditFunction>, String> {
+        let additions = self.convert_functions_additions(&changes.additions)?;
+        let edits: HashMap<usize, EditFunction> = changes.edits.iter().map(|(i, (o, m))| {
+            Ok((*i, EditFunction {
+                name: edit_field(&self.resolve_string_ref(&o.name)?, &self.resolve_string_ref(&m.name)?),
+            }))
+        }).collect::<Result<HashMap<_, _>, String>>()?;
+        
+        Ok(EditUnorderedList { additions, edits })
     }
 }
 
--- src/export_mod/sounds.rs
+++ src/export_mod/sounds.rs
@@ -1,12 +1,35 @@
 use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use crate::deserialize::all::GMData;
 use crate::deserialize::sounds::{GMSound, GMSoundFlags};
 use crate::export_mod::export::{edit_field, edit_field_option, flag_field, GModData, ModUnorderedRef};
-use crate::export_mod::unordered_list::{ AModUnorderedListChanges, GModUnorderedListChanges};
+use crate::export_mod::unordered_list::{export_changes_unordered_list, EditUnorderedList, GModUnorderedListChanges};
+
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ModSound {
+pub struct AddSound {
+    pub name: ModUnorderedRef,      // String
+    pub flags: AddSoundFlags,
+    pub audio_type: ModUnorderedRef,  // String
+    pub file: ModUnorderedRef,      // String
+    pub effects: u32,
+    pub volume: f32,
+    pub pitch: f32,
+    pub audio_file: ModUnorderedRef,    // Embedded Audio
+    pub audio_length: f32,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AddSoundFlags {
+    pub is_embedded: bool,
+    pub is_compressed: bool,
+    pub is_decompressed_on_load: bool,
+    pub regular: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EditSound {
     pub name: Option<ModUnorderedRef>,      // String
-    pub flags: Option<ModSoundFlags>,
+    pub flags: Option<EditSoundFlags>,
     pub audio_type: Option<ModUnorderedRef>,  // String
     pub file: Option<ModUnorderedRef>,      // String
     pub effects: Option<u32>,
@@ -15,92 +38,98 @@ pub struct ModSound {
     pub audio_file: Option<ModUnorderedRef>,    // Embedded Audio
     pub audio_length: Option<f32>,
 }
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ModSoundFlags {
+pub struct EditSoundFlags {
     pub is_embedded: Option<bool>,
     pub is_compressed: Option<bool>,
     pub is_decompressed_on_load: Option<bool>,
     pub regular: Option<bool>,
 }
 
-impl GModData<'_, '_> {
-    pub fn convert_sound_additions(&self, gm_sounds: &Vec<GMSound>) -> Result<Vec<ModSound>, String> {
-        let mut mod_sounds: Vec<ModSound> = Vec::with_capacity(gm_sounds.len());
 
-        for sound in gm_sounds {
-            mod_sounds.push(ModSound {
-                name: Some(self.resolve_string_ref(&sound.name)?),
-                flags: Some(self.convert_sound_flag_additions(&sound.flags)?),
-                audio_type: Some(self.resolve_string_ref(&sound.audio_type)?),
-                file: Some(self.resolve_string_ref(&sound.file)?),
-                effects: Some(sound.effects),
-                volume: Some(sound.volume),
-                pitch: Some(sound.pitch),
-                audio_file: if let Some(ref audio_file) = sound.audio_file { Some(self.resolve_audio_ref(&audio_file)?) } else { None },
-                audio_length: sound.audio_length,
-            });
-        }
+// impl GModData<'_, '_> {
+//     pub fn convert_sound_additions(&self, gm_sounds: &Vec<GMSound>) -> Result<Vec<EditSound>, String> {
+//         let mut mod_sounds: Vec<EditSound> = Vec::with_capacity(gm_sounds.len());
+// 
+//         for sound in gm_sounds {
+//             mod_sounds.push(EditSound {
+//                 name: self.resolve_string_ref(&sound.name)?,
+//                 flags: Some(self.convert_sound_flag_additions(&sound.flags)?),
+//                 audio_type: self.resolve_string_ref(&sound.audio_type)?,
+//                 file: self.resolve_string_ref(&sound.file)?,
+//                 effects: Some(sound.effects),
+//                 volume: Some(sound.volume),
+//                 pitch: Some(sound.pitch),
+//                 audio_file: if let Some(ref audio_file) = sound.audio_file { Some(self.resolve_audio_ref(&audio_file)?) } else { None },
+//                 audio_length: sound.audio_length,
+//             });
+//         }
+// 
+//         Ok(mod_sounds)
+//     }
+// 
+//     pub fn convert_sounds(&self, changes: &GModUnorderedListChanges<GMSound>) -> Result<EditUnorderedList<EditSound>, String> {
+//         let additions: Vec<EditSound> = self.convert_sound_additions(&changes.additions)?;
+//         let mut edits: HashMap<usize, EditSound> = HashMap::new();
+// 
+//         // let fn_audio_not_set: fn(&GMSound) -> String = |sound: &GMSound| format!(
+//         //     "Audio data not set for Sound \"{}\"! AcornGM ",
+//         //     sound.name.display(&self.modified_data.strings),
+//         // );
+// 
+//         for (index, (original, modified)) in &changes.edits {
+//             let resolved_original_audio_file = original
+//                 .audio_file
+//                 .as_ref()
+//                 .map(|def| self.resolve_audio_ref(def))
+//                 .transpose()?; // This gives you an Option<T>, not Result<Option<T>>
+// 
+//             let resolved_modified_audio_file = modified
+//                 .audio_file
+//                 .as_ref()
+//                 .map(|def| self.resolve_audio_ref(def))
+//                 .transpose()?;
+// 
+//             edits.insert(*index, EditSound {
+//                 name: edit_field(&self.resolve_string_ref(&original.name)?, &self.resolve_string_ref(&modified.name)?),
+//                 flags: Some(self.convert_sound_flags(&original.flags, &modified.flags)?),
+//                 audio_type: edit_field(&self.resolve_string_ref(&original.audio_type)?, &self.resolve_string_ref(&modified.audio_type)?),
+//                 file: edit_field(&self.resolve_string_ref(&original.file)?, &self.resolve_string_ref(&modified.file)?),   // unnecessary ig
+//                 effects: edit_field(&original.effects, &modified.effects),
+//                 volume: edit_field(&original.volume, &modified.volume),
+//                 pitch: edit_field(&original.pitch, &modified.pitch),
+//                 audio_file: edit_field_option(&resolved_original_audio_file, &resolved_modified_audio_file).clone(),
+//                 audio_length: edit_field_option(&original.audio_length, &modified.audio_length),
+//             });
+//         }
+// 
+//         Ok(EditUnorderedList { additions, edits })
+//     }
+// 
+//     pub fn convert_sound_flag_additions(&self, flags: &GMSoundFlags) -> Result<EditSoundFlags, String> {
+//         let flags = EditSoundFlags {
+//             is_embedded: Some(flags.is_embedded),
+//             is_compressed: Some(flags.is_compressed),
+//             is_decompressed_on_load: Some(flags.is_decompressed_on_load),
+//             regular: Some(flags.regular),
+//         };
+//         Ok(flags)
+//     }
+// 
+//     pub fn convert_sound_flags(&self, original: &GMSoundFlags, modified: &GMSoundFlags) -> Result<EditSoundFlags, String> {
+//         let flags = EditSoundFlags {
+//             is_embedded: flag_field(original.is_embedded, modified.is_embedded),
+//             is_compressed: flag_field(original.is_compressed, modified.is_compressed),
+//             is_decompressed_on_load: flag_field(original.is_decompressed_on_load, modified.is_decompressed_on_load),
+//             regular: flag_field(original.regular, modified.regular),
+//         };
+//         Ok(flags)
+//     }
+// }
 
-        Ok(mod_sounds)
-    }
 
-    pub fn convert_sounds(&self, changes: &GModUnorderedListChanges<GMSound>) -> Result<AModUnorderedListChanges<ModSound>, String> {
-        let additions: Vec<ModSound> = self.convert_sound_additions(&changes.additions)?;
-        let mut edits: HashMap<usize, ModSound> = HashMap::new();
-
-        // let fn_audio_not_set: fn(&GMSound) -> String = |sound: &GMSound| format!(
-        //     "Audio data not set for Sound \"{}\"! AcornGM ",
-        //     sound.name.display(&self.modified_data.strings),
-        // );
-
-        for (index, (original, modified)) in &changes.edits {
-            let resolved_original_audio_file = original
-                .audio_file
-                .as_ref()
-                .map(|def| self.resolve_audio_ref(def))
-                .transpose()?; // This gives you an Option<T>, not Result<Option<T>>
-
-            let resolved_modified_audio_file = modified
-                .audio_file
-                .as_ref()
-                .map(|def| self.resolve_audio_ref(def))
-                .transpose()?;
-
-            edits.insert(*index, ModSound {
-                name: edit_field(&self.resolve_string_ref(&original.name)?, &self.resolve_string_ref(&modified.name)?),
-                flags: Some(self.convert_sound_flags(&original.flags, &modified.flags)?),
-                audio_type: edit_field(&self.resolve_string_ref(&original.audio_type)?, &self.resolve_string_ref(&modified.audio_type)?),
-                file: edit_field(&self.resolve_string_ref(&original.file)?, &self.resolve_string_ref(&modified.file)?),   // unnecessary ig
-                effects: edit_field(&original.effects, &modified.effects),
-                volume: edit_field(&original.volume, &modified.volume),
-                pitch: edit_field(&original.pitch, &modified.pitch),
-                audio_file: edit_field_option(&resolved_original_audio_file, &resolved_modified_audio_file).clone(),
-                audio_length: *edit_field_option(&original.audio_length, &modified.audio_length),
-            });
-        }
-
-        Ok(AModUnorderedListChanges { additions, edits })
-    }
-
-    pub fn convert_sound_flag_additions(&self, flags: &GMSoundFlags) -> Result<ModSoundFlags, String> {
-        let flags = ModSoundFlags {
-            is_embedded: Some(flags.is_embedded),
-            is_compressed: Some(flags.is_compressed),
-            is_decompressed_on_load: Some(flags.is_decompressed_on_load),
-            regular: Some(flags.regular),
-        };
-        Ok(flags)
-    }
-
-    pub fn convert_sound_flags(&self, original: &GMSoundFlags, modified: &GMSoundFlags) -> Result<ModSoundFlags, String> {
-        let flags = ModSoundFlags {
-            is_embedded: flag_field(original.is_embedded, modified.is_embedded),
-            is_compressed: flag_field(original.is_compressed, modified.is_compressed),
-            is_decompressed_on_load: flag_field(original.is_decompressed_on_load, modified.is_decompressed_on_load),
-            regular: flag_field(original.regular, modified.regular),
-        };
-        Ok(flags)
-    }
+pub fn export_sounds(original_data: &GMData, modified_data: &GMData) -> Result<a, String> {
+    let a = export_changes_unordered_list(&original_data.sounds.sounds_by_index, &modified_data.sounds.sounds_by_index)?;
 }
+
 
