2d75a3b320d3c3d58fdb2b20e63c8ca663a8b1bf
BioTomateDE <biotomatede@proton.me>
2025-06-12 13:38:46 +0200

    main.rs main_export_mod

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,61 @@ use log::{info, error};
 
 use crate::deserialize::all::{parse_data_file, read_data_file};
 use crate::deserialize::all::GMData;
+use crate::export_mod::export::export_mod;
 use crate::serialize::all::{build_data_file, write_data_file};
 
 
-fn open_and_close() -> Result<(), String> {
+fn main_open_and_close() -> Result<(), String> {
     let args: Vec<String> = std::env::args().collect();
     let original_data_file_path: &Path = Path::new(args.get(1).map_or("data.win", |s| s));
     
     info!("Loading data file \"{}\"", original_data_file_path.display());
-    let original_data: Vec<u8> = read_data_file(original_data_file_path)
+    let original_data_raw: Vec<u8> = read_data_file(original_data_file_path)
         .map_err(|e| format!("Error while reading data file: {e}"))?;
 
     info!("Parsing data file");
-    let data: GMData = parse_data_file(original_data)
+    let original_data: GMData = parse_data_file(original_data_raw)
         .map_err(|e| format!("Error while parsing data file: {e}"))?;
 
     info!("Building data file");
-    let modded_data: Vec<u8> = build_data_file(&data)
+    let modified_data_raw: Vec<u8> = build_data_file(&original_data)
         .map_err(|e| format!("Error while building data file: {e}"))?;
 
     let modified_data_file_path: &Path = Path::new("./data_out.win");
     info!("Writing data file \"{}\"", modified_data_file_path.display());
-    write_data_file(modified_data_file_path, &modded_data)
+    write_data_file(modified_data_file_path, &modified_data_raw)
         .map_err(|e| format!("Error while writing data file: {e}"))?;
     
+    Ok(())
+}
+
+
+fn main_export_mod() -> Result<(), String> {
+    let args: Vec<String> = std::env::args().collect();
+    let original_data_file_path: &Path = Path::new(args.get(1).map_or("data_original.win", |s| s));
+    let modified_data_file_path: &Path = Path::new(args.get(2).map_or("data_modified.win", |s| s));
+    let mod_data_path: &Path = Path::new(args.get(3).map_or("mod.acornmod", |s| s));
+
+    info!("Loading original data file \"{}\"", original_data_file_path.display());
+    let original_data_raw: Vec<u8> = read_data_file(original_data_file_path)
+        .map_err(|e| format!("Error while reading data file: {e}"))?;
+
+    info!("Parsing original data file");
+    let original_data: GMData = parse_data_file(original_data_raw)
+        .map_err(|e| format!("Error while parsing original data file: {e}"))?;
+
+    info!("Loading modified data file \"{}\"", modified_data_file_path.display());
+    let modified_data_raw: Vec<u8> = read_data_file(modified_data_file_path)
+        .map_err(|e| format!("Error while reading data file: {e}"))?;
+
+    info!("Parsing modified data file");
+    let modified_data: GMData = parse_data_file(modified_data_raw)
+        .map_err(|e| format!("Error while parsing modified data file: {e}"))?;
+    
+    info!("Extracting changes and exporting mod to file \"{}\"", mod_data_path.display());
+    export_mod(&original_data, &modified_data, mod_data_path)
+        .map_err(|e| format!("Error while exporting mod: {e}"))?;
+
     Ok(())
 }
 
@@ -42,7 +73,7 @@ fn open_and_close() -> Result<(), String> {
 fn main() {
     biologischer_log::init(env!("CARGO_PKG_NAME"));
     
-    if let Err(e) = open_and_close() {
+    if let Err(e) = main_open_and_close() {
         error!("{e}");
         exit(1);
     }
