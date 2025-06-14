769bfd6fc209f8282217da2f830d7856ae4b3782
BioTomateDE <latus.kati@gmail.com>
2025-06-14 19:22:00 +0200

    export audio

---
+++ src/export_mod/audio.rs
@@ -0,0 +1,15 @@
+use crate::export_mod::export::{convert_additions, ModExporter};
+use crate::export_mod::unordered_list::{export_changes_unordered_list, EditUnorderedList};
+
+impl ModExporter<'_, '_> {
+    // TODO potential optimization by using references to the data
+    pub fn export_audios(&self) -> Result<EditUnorderedList<Vec<u8>, Vec<u8>>, String> {
+        export_changes_unordered_list(
+            &self.original_data.audios.audios_by_index,
+            &self.modified_data.audios.audios_by_index,
+            |i| Ok(i.raw_data.to_vec()),
+            |_, m| Ok(m.raw_data.to_vec()),
+        )
+    }
+}
+
