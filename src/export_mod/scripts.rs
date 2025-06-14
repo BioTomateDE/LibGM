5d216ed5728e0bd2a47e20ee281d8be1e88fd4a3
BioTomateDE <biotomatede@proton.me>
2025-06-14 20:27:02 +0200

    export all untested

---
+++ src/export_mod/scripts.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+use crate::export_mod::export::{ModExporter, ModRef};
+use crate::export_mod::unordered_list::{export_changes_unordered_list, EditUnorderedList};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ModScript {
+    pub name: ModRef,
+    pub is_constructor: bool,
+    pub code: Option<ModRef>,
+}
+
+
+impl ModExporter<'_, '_> {
+    pub fn export_scripts(&self) -> Result<EditUnorderedList<ModScript, ModScript>, String> {
+        export_changes_unordered_list(
+            &self.original_data.scripts.scripts_by_index,
+            &self.modified_data.scripts.scripts_by_index,
+            |i| Ok(ModScript {
+                name: self.convert_string_ref(i.name)?,
+                is_constructor: i.is_constructor,
+                code: self.convert_code_ref_opt(i.code)?,
+            }),
+            |_, m| Ok(ModScript {
+                name: self.convert_string_ref(m.name)?,
+                is_constructor: m.is_constructor,
+                code: self.convert_code_ref_opt(m.code)?,
+            }),
+        )
+    }
+}
+
