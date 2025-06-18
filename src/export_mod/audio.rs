453b544de564cdd02f49f0d8633600cd6994a730
BioTomateDE <biotomatede@proton.me>
2025-06-19 01:02:15 +0200

    texturesx

--- src/export_mod/audio.rs
+++ src/export_mod/audio.rs
@@ -5,10 +5,10 @@ impl ModExporter<'_, '_> {
     // TODO potential optimization by using references to the data
     pub fn export_audios(&self) -> Result<EditUnorderedList<Vec<u8>, Vec<u8>>, String> {
         export_changes_unordered_list(
-            &self.original_data.audios.audios_by_index,
-            &self.modified_data.audios.audios_by_index,
-            |i| Ok(i.raw_data.to_vec()),
-            |_, m| Ok(m.raw_data.to_vec()),
+            &self.original_data.audios.audios,
+            &self.modified_data.audios.audios,
+            |i| Ok(i.audio_data.to_vec()),
+            |_, m| Ok(m.audio_data.to_vec()),
             false,
         )
     }
