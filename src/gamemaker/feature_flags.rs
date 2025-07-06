e783f7b6e037e58395aba8a065b0eef8768870e6
BioTomateDE <latus.kati@gmail.com>
2025-07-06 16:02:10 +0200

    ALL CHUNKS!!!

---
+++ src/gamemaker/feature_flags.rs
@@ -0,0 +1,33 @@
+use crate::gm_deserialize::{DataReader, GMChunkElement, GMElement, GMRef};
+use crate::gm_serialize::DataBuilder;
+
+#[derive(Debug, Clone)]
+pub struct GMFeatureFlags {
+    pub feature_flags: Vec<GMRef<String>>,
+    pub exists: bool,
+}
+
+impl GMChunkElement for GMFeatureFlags {
+    fn empty() -> Self {
+        Self { feature_flags: vec![], exists: false }
+    }
+    fn exists(&self) -> bool {
+        self.exists
+    }
+}
+
+impl GMElement for GMFeatureFlags {
+    fn deserialize(reader: &mut DataReader) -> Result<Self, String> {
+        reader.align(4)?;
+        let feature_flags: Vec<GMRef<String>> = reader.read_simple_list_of_strings()?;
+        Ok(Self { feature_flags, exists: true })
+    }
+
+    fn serialize(&self, builder: &mut DataBuilder) -> Result<(), String> {
+        if !self.exists { return Ok(()) }
+        builder.align(4);
+        builder.write_simple_list_of_strings(&self.feature_flags)?;
+        Ok(())
+    }
+}
+
