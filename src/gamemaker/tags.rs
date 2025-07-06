a985fc9ab63b590306020ab55cc15cb7f4ee1905
BioTomateDE <latus.kati@gmail.com>
2025-07-06 15:27:53 +0200

    tags hashmap

--- src/gamemaker/tags.rs
+++ src/gamemaker/tags.rs
@@ -1,16 +1,16 @@
+use std::collections::HashMap;
 use crate::gm_deserialize::{DataReader, GMChunkElement, GMElement, GMRef};
 use crate::gm_serialize::DataBuilder;
 
 #[derive(Debug, Clone)]
 pub struct GMTags {
     pub tags: Vec<GMRef<String>>,
-    pub asset_tags: Vec<GMAssetTags>,
-    // TODO: change this to a hashmap (before doing export logic)
+    pub asset_tags: HashMap<i32, Vec<GMRef<String>>>,
     pub exists: bool,
 }
 impl GMChunkElement for GMTags {
     fn empty() -> Self {
-        Self { tags: vec![], asset_tags: vec![], exists: false }
+        Self { tags: vec![], asset_tags: HashMap::new(), exists: false }
     }
     fn exists(&self) -> bool {
         self.exists
@@ -25,7 +25,14 @@ impl GMElement for GMTags {
             return Err(format!("Expected TAGS version 1 but got {version}"))
         }
         let tags: Vec<GMRef<String>> = reader.read_simple_list_of_strings()?;
-        let asset_tags: Vec<GMAssetTags> = reader.read_pointer_list()?;
+        let temp_asset_tags: Vec<TempAssetTags> = reader.read_pointer_list()?;
+
+        let mut asset_tags: HashMap<i32, Vec<GMRef<String>>> = HashMap::new();
+        for temp_asset_tag in temp_asset_tags {
+            if asset_tags.insert(temp_asset_tag.id, temp_asset_tag.tags).is_some() {
+                return Err(format!("Duplicate Asset ID {} while parsing Tags", temp_asset_tag.id))
+            }
+        }
         Ok(Self { tags, asset_tags, exists: true })
     }
 
@@ -34,18 +41,23 @@ impl GMElement for GMTags {
         builder.align(4);
         builder.write_i32(1);   // TAGS version
         builder.write_simple_list_of_strings(&self.tags)?;
-        builder.write_pointer_list(&self.asset_tags)?;
+        let temp_asset_tags: Vec<TempAssetTags> = self.asset_tags
+            .clone()
+            .into_iter()
+            .map(|(id, tags)| TempAssetTags {id, tags})
+            .collect();
+        builder.write_pointer_list(&temp_asset_tags)?;
         Ok(())
     }
 }
 
 
 #[derive(Debug, Clone)]
-pub struct GMAssetTags {
-    pub id: i32,
-    pub tags: Vec<GMRef<String>>,
+struct TempAssetTags {
+    id: i32,
+    tags: Vec<GMRef<String>>,
 }
-impl GMElement for GMAssetTags {
+impl GMElement for TempAssetTags {
     fn deserialize(reader: &mut DataReader) -> Result<Self, String> {
         let id: i32 = reader.read_i32()?;
         let tags: Vec<GMRef<String>> = reader.read_simple_list_of_strings()?;
