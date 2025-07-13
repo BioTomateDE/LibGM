c02982ae1ef20538987479e4b3444a620b153c01
BioTomateDE <biotomatede@proton.me>
2025-07-13 10:09:35 +0200

    variable parsing maybe a little faster

--- src/gamemaker/elements/variables.rs
+++ src/gamemaker/elements/variables.rs
@@ -32,38 +32,48 @@ impl GMElement for GMVariables {
         let variable_count: usize = reader.get_chunk_length() / variables_length;
         let scuffed: Option<GMVariablesScuffed> = reader.deserialize_if_bytecode_version(15)?;
 
+        let mut occurrence_infos: Vec<(usize, u32)> = Vec::with_capacity(variable_count);
         let mut variables: Vec<GMVariable> = Vec::with_capacity(variable_count);
-        let mut cur_index: u32 = 0;
 
+        // parse variables
         while reader.cur_pos + variables_length <= reader.chunk.end_pos {
             let name: GMRef<String> = reader.read_gm_string()?;
 
             let b15_data: Option<GMVariableB15Data> = reader.deserialize_if_bytecode_version(15)?;
 
-            let occurrences_count: i32 = reader.read_i32()?;
-            let occurrences_count: usize = if occurrences_count < 0 { 0 } else { occurrences_count as usize };
-            let first_occurrence_address: i32 = reader.read_i32()?;
+            let occurrence_count: i32 = reader.read_i32()?;
+            let occurrence_count: usize = if occurrence_count < 0 { 0 } else { occurrence_count as usize };
+            let first_occurrence_pos: u32 = reader.read_u32()?;
+            occurrence_infos.push((occurrence_count, first_occurrence_pos));
 
-            let (occurrences, name_string_id): (Vec<usize>, u32) = parse_occurrence_chain(reader, first_occurrence_address, occurrences_count)?;
+            variables.push(GMVariable { name, b15_data, name_string_id: 696969 });
+        }
+        
+        // resolve occurrences
+        let saved_chunk: GMChunk = reader.chunk.clone();
+        let saved_position: usize = reader.cur_pos;
+        reader.chunk = reader.chunks.get("CODE").cloned().ok_or("Chunk CODE not set while parsing variable occurrences")?;
+        
+        for (i, (occurrence_count, first_occurrence_pos)) in occurrence_infos.into_iter().enumerate() {
+            let (occurrences, name_string_id): (Vec<usize>, u32) = parse_occurrence_chain(reader, first_occurrence_pos, occurrence_count)?;
+            variables[i].name_string_id = name_string_id;
 
             for occurrence in occurrences {
-                if let Some(old_value) = reader.variable_occurrence_map.insert(occurrence, GMRef::new(cur_index)) {
+                if let Some(old_value) = reader.variable_occurrence_map.insert(occurrence, GMRef::new(i as u32)) {
                     return Err(format!(
-                        "Conflicting occurrence positions while parsing variables: absolute position {} \
-                        was already set for {}variable #{} with name \"{}\"; trying to set to variable #{} with name \"{}\"",
+                        "Conflicting occurrence positions while parsing variables: absolute position {} was already \
+                        set for variable #{} with name \"{}\"; trying to set to variable #{i} with name \"{}\"",
                         occurrence,
-                        b15_data.map_or_else(|| "".to_string(), |i| format!("{} ", i.instance_type)),
                         old_value.index,
-                        reader.display_gm_str(old_value.resolve(&variables)?.name),
-                        cur_index,
-                        reader.display_gm_str(name),
+                        reader.resolve_gm_str(old_value.resolve(&variables)?.name)?,
+                        reader.resolve_gm_str(variables[i].name)?,
                     ))
                 }
             }
-
-            variables.push(GMVariable { name, b15_data, name_string_id });
-            cur_index += 1;
         }
+
+        reader.chunk = saved_chunk;
+        reader.cur_pos = saved_position;
 
         Ok(GMVariables { variables, scuffed, yyc: false, exists: true })
     }
@@ -140,18 +150,14 @@ impl GMElement for GMVariablesScuffed {
 }
 
 
-pub fn parse_occurrence_chain(reader: &mut DataReader, first_occurrence_pos: i32, occurrence_count: usize) -> Result<(Vec<usize>, u32), String> {
+pub fn parse_occurrence_chain(reader: &mut DataReader, first_occurrence_pos: u32, occurrence_count: usize) -> Result<(Vec<usize>, u32), String> {
     if occurrence_count < 1 {
-        return Ok((vec![], first_occurrence_pos as u32));
+        return Ok((vec![], first_occurrence_pos));
     }
-
-    let saved_chunk: GMChunk = reader.chunk.clone();
-    let saved_position: usize = reader.cur_pos;
-    reader.chunk = reader.chunks.get("CODE").cloned().ok_or("Chunk CODE not set while parsing variable occurrences")?;
     
     let mut occurrence_pos: usize = first_occurrence_pos as usize + 4;
     let mut occurrences: Vec<usize> = vec_with_capacity(occurrence_count)?;
-    let mut offset: i32 = first_occurrence_pos;
+    let mut offset: i32 = 6969;   // default value will never be relevant since it returns if no occurrences
 
     for _ in 0..occurrence_count {
         occurrences.push(occurrence_pos);
@@ -169,8 +175,6 @@ pub fn parse_occurrence_chain(reader: &mut DataReader, first_occurrence_pos: i32
     }
 
     let name_string_id: u32 = (offset & 0xFFFFFF) as u32;
-    reader.chunk = saved_chunk;
-    reader.cur_pos = saved_position;
     Ok((occurrences, name_string_id))
 }
 
