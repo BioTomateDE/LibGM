760966281c0e1cbfd1614d4e47e5f574d4f54fe9
BioTomateHP <latus.kati@gmail.com>
2025-09-02 17:01:08 +0300

    PROBLEM: ut 1.01 has script with leftover stack data on impossible branch

--- tests/code_validation.rs
+++ tests/code_validation.rs
@@ -5,8 +5,7 @@ fn test_code_validation() -> Result<(), String> {
     use libgm::__test_data_files;
 
     __test_data_files(|data| {
-        //todo remove debuug
-        for (i, code) in data.codes.codes[34..].iter().enumerate() {
+        for (i, code) in data.codes.codes.iter().enumerate() {
             let code_name = code.name.resolve(&data.strings.strings)?;
             print!("({}/{}) Validating: {:<64}\n", i+1, data.codes.codes.len(), code_name);
             validate_code(code, &data).map_err(|e| format!("{e}\n↳ while validating code {code_name}"))?;
