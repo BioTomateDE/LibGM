1e95db6fd85a8f13115db7efcdb6c35bed30295f
BioTomateDE <latus.kati@gmail.com>
2025-09-23 09:01:06 +0200

    decompiler: fragments

---
+++ src/gml/decompiler/control_flow/blocks.rs
@@ -0,0 +1,185 @@
+use crate::gamemaker::elements::code::{get_instruction_size, GMInstruction};
+use crate::gml::decompiler::control_flow_graph::{ControlFlowGraph, NodeIndex, Successors};
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Clone)]
+pub struct Block<'a> {
+    pub instructions: &'a [GMInstruction],
+    pub start_address: u32,
+    pub end_address: u32,
+    pub block_index: usize,
+    pub predecessors: Vec<NodeIndex>,
+    pub successors: Successors,
+}
+
+
+impl<'a> Block<'a> {
+    pub fn pop_last_instruction(&mut self) {
+        self.instructions = &self.instructions[0..self.instructions.len() - 1];
+    }
+}
+
+
+fn generate_address_map(instructions: &[GMInstruction]) -> HashMap<u32, usize> {
+    // TODO: use vec with binary searching for performance
+    let mut map: HashMap<u32, usize> = HashMap::with_capacity(instructions.len());
+    let mut current_address: u32 = 0;
+
+    for (i, instruction) in instructions.iter().enumerate() {
+        map.insert(current_address, i);
+        current_address += get_instruction_size(instruction);
+    }
+
+    // Insert end block
+    map.insert(current_address, instructions.len());
+    map
+}
+
+
+pub fn find_blocks<'a>(cfg: &mut ControlFlowGraph<'a>, instructions: &'a [GMInstruction]) {
+    if instructions.is_empty() {
+        return
+    }
+
+    // 1. Generate address map
+    let address_map: HashMap<u32, usize> = generate_address_map(instructions);
+
+    // 2. Get nodes and branch table
+    let mut current_address: u32 = 0;
+    let mut nodes_set: HashSet<u32> = HashSet::new();
+    let mut branch_table: HashMap<usize, u32> = HashMap::new();
+
+    for (current_index, instruction) in instructions.iter().enumerate() {
+        match instruction {
+            GMInstruction::Exit(_) |
+            GMInstruction::Return(_) => {
+                nodes_set.insert(current_address + 1);
+            }
+            GMInstruction::Branch(instr) |
+            GMInstruction::BranchIf(instr) |
+            GMInstruction::BranchUnless(instr) |
+            GMInstruction::PushWithContext(instr) |
+            GMInstruction::PopWithContext(instr) => {
+                let target_address: u32 = (current_address as i32 + instr.jump_offset) as u32;
+                branch_table.insert(current_index, target_address);
+                nodes_set.insert(current_address + 1);
+                nodes_set.insert(target_address);
+            }
+            // TODO: handle call to @@try_hook@@ function
+            _ => {}
+        }
+        current_address += get_instruction_size(instruction);
+    }
+    let code_end_address: u32 = current_address;
+
+    // Push end node, convert nodes to sorted vec
+    nodes_set.insert(instructions.len() as u32);
+    let mut nodes_list: Vec<u32> = nodes_set.into_iter().collect();
+    nodes_list.sort_unstable();
+
+    // 3. Create blocks
+    cfg.blocks = Vec::with_capacity(nodes_list.len());
+    let mut current_address: u32 = 0;
+    let mut current_index: usize = 0;
+
+    for instruction_address in nodes_list {
+        if instruction_address > current_address {
+            let instruction_index = address_map[&instruction_address];
+            cfg.blocks.push(Block {
+                instructions: &instructions[current_index..instruction_index],
+                start_address: current_address,
+                end_address: instruction_address,
+                block_index: cfg.blocks.len(),
+                predecessors: vec![],
+                successors: Successors::None,
+            });
+            current_index = instruction_index;
+        }
+        current_address = instruction_address;
+    }
+
+    // Add end block
+    let block_count = cfg.blocks.len();
+    cfg.blocks.push(Block {
+        instructions: &[],
+        start_address: code_end_address,
+        end_address: code_end_address,
+        block_index: block_count,
+        predecessors: vec![],
+        successors: Successors::None,
+    });
+
+    // 4. Populate predecessor and successor fields of blocks
+    for block_index in 0..block_count {
+        let block = &cfg.blocks[block_index];
+        let last_instruction: &GMInstruction = block.instructions.last().unwrap();
+        let last_instruction_address: u32 = block.end_address - 1;
+
+        // TODO: handle call to @@try_hook@@ function (PopDiscard is relevant)
+
+        match last_instruction {
+            // This terminates code execution, so this block won't have any successors
+            GMInstruction::Exit(_) | GMInstruction::Return(_) => continue,
+
+            // Insert branch target to predecessor/successor list
+            GMInstruction::Branch(instr) |
+            GMInstruction::BranchIf(instr) |
+            GMInstruction::BranchUnless(instr) |
+            GMInstruction::PushWithContext(instr) |
+            GMInstruction::PopWithContext(instr) => {
+                let target_address: u32 = (last_instruction_address as i32 + instr.jump_offset) as u32;
+                if target_address == code_end_address {
+                    // If instruction branches to the end of the code, insert fictional end block
+                    cfg.connect_nodes(NodeIndex::Block(block_index), NodeIndex::Block(block_count));
+                } else {
+                    let target_block_index: usize = get_block_index(&cfg.blocks, target_address);
+                    cfg.connect_nodes(NodeIndex::Block(block_index), NodeIndex::Block(target_block_index));
+                }
+            }
+            _ => {}
+        }
+
+        // Only add sequential successor block if the block is not the last one
+        if block_index < cfg.blocks.len() - 1 {
+            cfg.blocks[block_index + 1].predecessors.push(NodeIndex::Block(block_index));
+            cfg.blocks[block_index].successors.set_sequential_next();
+        }
+    }
+}
+
+
+/// Convert an instruction address to a block index.
+/// In other words, find the block this instruction belongs to.
+/// # Panic Safety
+/// This function will panic if `blocks` is empty, unordered,
+/// or if the block `start_index`/`end_index` fields are malformed.
+fn get_block_index(blocks: &[Block], instruction_address: u32) -> usize {
+    if blocks.is_empty() {
+        unreachable!("Cannot find block in empty blocks array")
+    }
+
+    let mut low: usize = 0;
+    let mut high: usize = blocks.len() - 1;
+
+    while low <= high {
+        let mid: usize = (low + high) / 2;
+        let block: &Block = &blocks[mid];
+
+        if instruction_address < block.start_address {
+            high = mid - 1;
+        } else if instruction_address >= block.end_address {
+            low = mid + 1;
+        } else {
+            return mid
+        }
+    }
+
+    unreachable!(
+        "Could not find block for instruction index {} in {} blocks (range: {}-{})",
+        instruction_address,
+        blocks.len(),
+        blocks[0].start_address,
+        blocks.last().unwrap().end_address,
+    );
+}
+
---
+++ src/gml/decompiler/control_flow/fragments.rs
@@ -0,0 +1,162 @@
+use crate::gamemaker::data::GMData;
+use crate::gamemaker::deserialize::GMRef;
+use crate::gamemaker::elements::code::{get_instruction_size, GMCode, GMInstruction};
+use crate::gml::decompiler::control_flow_graph::{ControlFlowGraph, NodeIndex, Successors};
+use crate::utility::SmallMap;
+
+#[derive(Debug, Clone)] // TODO: mutable blocks but more cancer
+pub struct Fragment<'a> {
+    pub start_address: u32,
+
+    pub end_address: u32,
+
+    pub children: Vec<NodeIndex>,
+
+    /// The base blocks that this fragment is composed of.
+    pub blocks: Vec<NodeIndex>,
+
+    /// Code entry that this fragment belongs to.
+    pub code_entry: &'a GMCode,
+
+    /// Whether this fragment is a root-scope fragment (that is, it may be a global function when inside a global script).
+    pub root_scope: bool,
+
+    pub predecessors: Vec<NodeIndex>,
+
+    pub successors: Successors,
+}
+
+
+pub fn find_fragments<'a>(cfg: &'a mut ControlFlowGraph<'a>, gm_data: &'a GMData, code_ref: GMRef<GMCode>) -> Result<(), String> {
+    let child_start_offsets: SmallMap<u32, &GMCode> = get_child_start_offsets(gm_data, code_ref)?;
+    let code: &GMCode = code_ref.resolve(&gm_data.codes.codes)?;
+    let code_end_address: u32 = get_code_end_address(&code.instructions);
+
+    // Build fragments, using a stack to track hierarchy
+    let mut stack: Vec<Fragment> = Vec::new();
+    cfg.fragments = Vec::with_capacity(child_start_offsets.len());
+    let mut current = Fragment {
+        start_address: 0,
+        end_address: code_end_address,
+        blocks: vec![],
+        children: vec![],
+        code_entry: code,
+        root_scope: false,
+        predecessors: vec![],
+        successors: Successors::None,
+    };
+
+    for i in 0..cfg.blocks.len() {
+        let block = &mut cfg.blocks[i];
+        // Check if our current fragment is ending at this block
+        if block.start_address == current.end_address {
+            if stack.is_empty() {
+                // We're done processing now. Add last block and exit loop.
+                current.children.push(NodeIndex::Block(i));
+                current.blocks.push(NodeIndex::Block(i));
+                if block.start_address != code_end_address {
+                    return Err(format!("Final block starts at address {} but should start at the code's end address {}", block.start_address, code_end_address))
+                }
+                break
+            }
+
+            // We're an inner fragment; remove the Exit Instruction
+            if let Some(NodeIndex::Block(idx)) = current.blocks.last() {
+                let last_block = &mut cfg.blocks[*idx];
+                match last_block.instructions.last() {
+                    Some(GMInstruction::Exit(_)) => (last_block).pop_last_instruction(),
+                    Some(instr) => return Err(format!("Expected Exit instruction; got {instr:?}")),
+                    None => unreachable!("Block doesn't have any instructions"),
+                }
+            } else {
+                return Err("fragment doesn't have any blocks or last block is not a block".to_string());
+            }
+
+            // Go to the fragment the next level up
+            current = stack.pop().expect("fragment stack is empty");
+        }
+
+        // Check for new fragment starting at this block
+        if let Some(child_code) = child_start_offsets.get(&cfg.blocks[i].start_address) {
+            // Our "current" is now the next level up
+            stack.push(current.clone());
+
+            // Compute the end address of this fragment, by looking at previous block
+            let previous = &mut cfg.blocks[i - 1];
+            let last_instr = previous.instructions.last().unwrap();
+            if !matches!(last_instr, GMInstruction::Branch(_)) {
+                return Err(format!("Expected unconditional branch instruction before fragment start, got {last_instr:?}"))
+            }
+            let Successors::UnconditionalBranch(branch_target_node) = previous.successors.clone() else {
+                unreachable!("Successor enum of previous block (which ends in a `Branch` instruction) is not `UnconditionalBranch`")
+            };
+
+            // Remove previous block's branch instruction
+            previous.pop_last_instruction();
+
+            // Make our new "current" be this new fragment
+            cfg.fragments.push(current);
+            current = Fragment {
+                start_address: cfg.blocks[i].start_address,
+                end_address: branch_target_node.start_address(cfg),
+                blocks: vec![],
+                children: vec![],
+                code_entry: child_code,
+                root_scope: stack.len() == 1,
+                predecessors: vec![],
+                successors: Successors::None,
+            };
+
+            // Rewire previous block to jump to this fragment, and this fragment
+            // to jump to the successor of the previous block.
+            let previous = &mut cfg.blocks[i - 1];
+            let cur_node_idx = NodeIndex::Fragment(cfg.fragments.len());
+            let prev_node_idx = NodeIndex::Block(i - 1);
+            previous.successors.set_branch_target(cur_node_idx.clone());
+            let predecessors = branch_target_node.predecessors_mut(cfg);
+            let pred_index: usize = predecessors.iter().position(|node| *node == prev_node_idx)
+                .ok_or(format!("Could not find predecessor with block index {} for branch target node", i-1))?;
+            predecessors[pred_index] = cur_node_idx;
+            current.predecessors.push(prev_node_idx);
+            current.successors.set_branch_target(branch_target_node);
+        }
+
+        current.children.push(NodeIndex::Block(i));
+        current.blocks.push(NodeIndex::Block(i));
+    }
+
+    if !stack.is_empty() {
+        return Err(format!("Failed to close all fragments; stack still has {} items", stack.len()))
+    }
+
+    cfg.fragments.push(current);
+    Ok(())
+}
+
+
+fn get_child_start_offsets(gm_data: &GMData, parent_code_ref: GMRef<GMCode>) -> Result<SmallMap<u32, &GMCode>, String> {
+    let mut start_offsets = SmallMap::new();
+    for code in &gm_data.codes.codes {
+        let Some(b15_info) = &code.bytecode15_info else {continue};
+        let Some(parent) = b15_info.parent else {continue};
+        if parent != parent_code_ref {
+            continue
+        }
+
+        if b15_info.offset % 4 != 0 {
+            return Err(format!("Child code instruction offset {} does not point to the start of an instruction", b15_info.offset))
+        }
+        start_offsets.insert(b15_info.offset / 4, code);
+    }
+    Ok(start_offsets)
+}
+
+
+fn get_code_end_address(instructions: &Vec<GMInstruction>) -> u32 {
+    let mut length: u32 = 0;
+    for instruction in instructions {
+        length += get_instruction_size(instruction);
+    }
+    length
+}
+
