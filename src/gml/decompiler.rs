7307da04e231393e233004777d45b2c913a716a2
BioTomateDE <latus.kati@gmail.com>
2025-09-28 23:16:21 +0200

    whatever this is

--- src/gml/decompiler.rs
+++ src/gml/decompiler.rs
@@ -8,6 +8,8 @@ use crate::gml::decompiler::control_flow::loops::find_loops;
 use crate::gml::decompiler::control_flow::short_circuits::find_short_circuits;
 use crate::gml::decompiler::control_flow::static_inits::find_static_inits;
 use crate::gml::decompiler::decompile_context::DecompileContext;
+use crate::gml::disassembler::disassemble_instruction;
+
 
 pub mod control_flow;
 pub mod decompile_context;
@@ -27,6 +29,18 @@ pub fn decompile_to_ast(gm_data: &GMData, code_ref: GMRef<GMCode>) -> Result<(),
 
     let code = code_ref.resolve(&gm_data.codes.codes)?;
     find_blocks(&mut cfg, &code.instructions)?;
+    for i in &cfg.blocks {
+        println!(
+            "{:>3}..{:<3} ({} | {})  {}",
+            i.start_address,
+            i.end_address,
+            i.instructions.first().map_or("///".to_string(), |x| disassemble_instruction(gm_data, x).unwrap_or("<?>".to_string())),
+            i.instructions.last().map_or("///".to_string(), |x| disassemble_instruction(gm_data, x).unwrap_or("<?>".to_string())),
+            i.instructions.len(),
+        )
+    }
+    // std::process::exit(67);
+
     find_fragments(&mut cfg, code_ref)?;
     find_static_inits(&mut cfg)?;
     find_short_circuits(&mut cfg);
